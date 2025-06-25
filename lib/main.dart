a7d6c9191861fd9ac1c1a0c08ac932711368ea70
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-25 14:21:01 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'e5gez/choice_page.dart';
 import 'gamma3/first_page.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "body",
-    home: FirstPage(),
+    home: ChoicePage(),
   ));
 }
