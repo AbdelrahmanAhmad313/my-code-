052173dea676b52366e8d9c76fb24c326393a064
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-25 13:54:50 +0300

    done

--- lib/a_shortcuts/text_input.dart
+++ lib/a_shortcuts/text_input.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-
 class TextInput extends StatelessWidget {
-  final int borderSide;
-  const TextInput({super.key,this.borderSide=0});
+
+  final double borderSide;
+  const TextInput(
+      {super.key,
+        this.borderSide = 0,
+      });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(borderSide),
 
+          )
       ),
     );
   }
