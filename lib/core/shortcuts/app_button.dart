d131fe11b8125d836a67f059561b6e594d073f55
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-26 22:00:43 +0300

    done

--- lib/core/shortcuts/app_button.dart
+++ lib/core/shortcuts/app_button.dart
@@ -1,14 +1,17 @@
 
 
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class appButton extends StatelessWidget {
   final String text;
-  final double width,height;
+  final double width,height,fontSize;
   final VoidCallback onPressed;
   final Color color;
   final BorderRadius borderRadius;
+  final FontWeight fontWeight;
   const appButton({super.key,
     this.text="",
     required this.onPressed,
@@ -16,13 +19,15 @@ class appButton extends StatelessWidget {
     required this.borderRadius,
     this.width=100,
     this.height=100,
+    this.fontSize=14,
+    this.fontWeight=FontWeight.w400,
   });
 
   @override
   Widget build(BuildContext context) {
     return FilledButton(
       onPressed: onPressed,
-      child:Text(text) ,
+      child:Text(text,style: TextStyle(fontSize: fontSize,fontWeight:fontWeight ),) ,
       style: FilledButton.styleFrom(
         backgroundColor: color,
         minimumSize: Size(width, height),
