4d0eebbd79e0a98995c3d62ad3c8fe0fbb7e6775
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-26 23:47:32 +0300

    done

--- lib/hagz/adding_field.dart
+++ lib/hagz/adding_field.dart
@@ -10,10 +10,29 @@ class AddingField extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.green,
       ),
-      body: appInput(
-        label: "العدد المطلوب",
-        textDirection: TextDirection.rtl,
-        borderradius: BorderRadius.circular(15),
+      body: Column(
+        children: [
+          SizedBox(height: 25,),
+          appInput(
+            label: "العدد المطلوب",
+            textDirection: TextDirection.rtl,
+            borderradius: BorderRadius.circular(25),
+          ),SizedBox(height: 25,),
+          appInput(
+            label: "عدد الساعات",
+            textDirection: TextDirection.rtl,
+            borderradius: BorderRadius.circular(25),
+          ),SizedBox(height: 25,),
+          appInput(
+            label: "المكان",
+            textDirection: TextDirection.rtl,
+            borderradius: BorderRadius.circular(25),
+          ),
+          SizedBox(height: 25,),
+          appInput(
+            keyboardtype: TextInputType.numberWithOptions(decimal: false,signed: true),
+          )
+        ],
       ),
     );
   }
