a3787cf36be5b586fd6e041d38c9ddfddad025ec
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-30 15:47:43 +0300

    done

--- lib/hagz/adding_field.dart
+++ lib/hagz/adding_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/core/shortcuts/app_button.dart';
 import 'package:myapp/core/shortcuts/app_input.dart';
-
+import 'package:alarm/alarm.dart';
 import 'paddle/paddle_way.dart';
 
 class AddingField extends StatefulWidget {
@@ -15,12 +15,9 @@ class AddingField extends StatefulWidget {
 class _AddingFieldState extends State<AddingField> {
   final TextEditingController plNum = TextEditingController();
   final TextEditingController place = TextEditingController();
-  late String playersNum;
-  late String location;
-   late TimeOfDay timeFrom ;
-   late TimeOfDay timeTo ;
-  TimeOfDay _selectedTime = TimeOfDay.now();
-  late TimeOfDay? pickedTime=TimeOfDay(hour: 12, minute: 00);
+  final TextEditingController timeFrom = TextEditingController();
+  final TextEditingController timeTo = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,12 +29,7 @@ class _AddingFieldState extends State<AddingField> {
       persistentFooterButtons: [
         appButton(
           onPressed: () {
-            playersNum = plNum.text;
-            plNum.clear();
-            location = place.text;
-            place.clear();
-            // timeFrom=selectedFrom;
-            // setState(() {});
+
           },
           borderColor: Colors.white,
           text: "تمام",
@@ -76,59 +68,36 @@ class _AddingFieldState extends State<AddingField> {
           SizedBox(
             height: 35,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              appButton(
-                width: 150,
-                height: 50,
-                color: Colors.green,
-                borderColor: Colors.white,
-                text: "إلي",
-                onPressed: ()async {
-                  TimeOfDay? picked = await showTimePicker(
-                    context: context,
-                    initialTime: timeTo,
-                  );
-                  if (picked != null && picked != timeTo) {
-                    setState(() {
-                      timeTo = picked;
-                    });
-                  }
-                },
-                // {
-                //    TimeOfDay selectedFrom=  showTimePicker(
-                //       context: context,
-                //       initialTime: TimeOfDay(hour: 12, minute: 0)
-                //   ) as TimeOfDay;
-                // },
+          TextFormField(
+            onTapOutside:(event) {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            readOnly: true,
+            controller: timeFrom,
+            style: TextStyle(
+              color: Colors.black
+            ),
+            decoration: InputDecoration(
+              label: Text("enter"),
+              fillColor: Colors.black,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15)
+              ),
+              enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(15),
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              appButton(
-                width: 150,
-                height: 50,
-                color: Colors.green,
-                borderColor: Colors.white,
-                text: "من",
-                onPressed: () {
-                  showTimePicker(
-                      context: context,
-                      initialTime: TimeOfDay(hour: 12, minute: 0));
-                },
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ],
+              )
+            ),
+            onTap: () async {
+              final selectedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (selectedTime != null) {
+                timeFrom.text = selectedTime.format(context);
+              }
+            }
           ),
-          SizedBox(
-            height: 25,
-          ),
-          // Text(
-          //   timeTo.format(context),
-          //   style: TextStyle(fontSize: 30),
-          // ),
+
         ],
       ),
     );
