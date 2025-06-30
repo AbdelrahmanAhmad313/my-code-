044bf524ed24af72bd6773764e4d7f88ae4e19ba
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-06-30 19:50:54 +0300

    done

--- lib/hagz/adding_field.dart
+++ lib/hagz/adding_field.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/core/shortcuts/app_button.dart';
 import 'package:myapp/core/shortcuts/app_input.dart';
-import 'package:alarm/alarm.dart';
 import 'paddle/paddle_way.dart';
 
 class AddingField extends StatefulWidget {
@@ -17,7 +15,11 @@ class _AddingFieldState extends State<AddingField> {
   final TextEditingController place = TextEditingController();
   final TextEditingController timeFrom = TextEditingController();
   final TextEditingController timeTo = TextEditingController();
-
+  late TimeOfDay? selectedTimeFrom;
+  late TimeOfDay? selectedTimeTo;
+  late int players;
+  late String location;
+  late TimeOfDay startTime,endTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +31,14 @@ class _AddingFieldState extends State<AddingField> {
       persistentFooterButtons: [
         appButton(
           onPressed: () {
-
+            players=int.tryParse(plNum.text)!;
+            plNum.clear();
+            location=place.text;
+            place.clear();
+            startTime=selectedTimeFrom!;
+            endTime=selectedTimeTo!;
+            timeTo.clear();
+            timeFrom.clear();
           },
           borderColor: Colors.white,
           text: "تمام",
@@ -68,34 +77,80 @@ class _AddingFieldState extends State<AddingField> {
           SizedBox(
             height: 35,
           ),
-          TextFormField(
-            onTapOutside:(event) {
-              FocusManager.instance.primaryFocus?.unfocus();
-            },
-            readOnly: true,
-            controller: timeFrom,
-            style: TextStyle(
-              color: Colors.black
-            ),
-            decoration: InputDecoration(
-              label: Text("enter"),
-              fillColor: Colors.black,
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Directionality(
+                textDirection: TextDirection.rtl,
+                child: Container(
+                  width: 150,
+                  child: TextFormField(
+                    onTapOutside:(event) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    readOnly: true,
+                    controller: timeFrom,
+                    style: TextStyle(
+                      color: Colors.black
+                    ),
+                    decoration: InputDecoration(
+                      label: Text("إلى"),
+                      fillColor: Colors.black,
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )
+                    ),
+                    onTap: () async {
+                      selectedTimeTo = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                      if (selectedTimeTo != null) {
+                        timeFrom.text = selectedTimeTo.toString();
+                      }
+                    }
+                  ),
+                ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-              )
-            ),
-            onTap: () async {
-              final selectedTime = await showTimePicker(
-                context: context,
-                initialTime: TimeOfDay.now(),
-              );
-              if (selectedTime != null) {
-                timeFrom.text = selectedTime.format(context);
-              }
-            }
+              Directionality(
+                textDirection: TextDirection.rtl,
+                child: Container(
+                  width: 150,
+                  child: TextFormField(
+                    onTapOutside:(event) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    readOnly: true,
+                    controller: timeTo,
+                    style: TextStyle(
+                      color: Colors.black
+                    ),
+                    decoration: InputDecoration(
+                      label: Text("من"),
+                      fillColor: Colors.black,
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )
+                    ),
+                    onTap: () async {
+                       selectedTimeFrom = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                      if (selectedTimeFrom != null) {
+                        timeTo.text = selectedTimeFrom.toString();
+                      }
+                    }
+                  ),
+                ),
+              ),
+            ],
           ),
 
         ],
